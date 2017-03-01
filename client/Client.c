3238cd4ed35db5f870e0dda47ec30cb70d410506
Jan Joseph Concepcion <jconcep2@mail.sfsu.edu>
2017-02-28 17:00:53 -0800

    Update Client
    -Provide menu options
    -Connect to server when explicitly selected by user

--- client/Client.c
+++ client/Client.c
@@ -7,74 +7,90 @@
 
 #define RCVBUFSIZE 32 /* Size of receive buffer */
 
-void DieWithError(char *errorMessage); /* Error handling function */
+void DieWithError(char *errorMessage);
+void PrintMenuOptions();
+void ConnectToServer(int sock, struct sockaddr_in serverAddr);
 
 int main(int argc, char *argv[])
 {
   int sock; /* Socket descriptor */
-  struct sockaddr_in echoServAddr; /* Echo server address */
-  unsigned short echoServPort; /* Echo server port */
-  char *servlP; /* Server IP address (dotted quad) */
-  char *echoString; /* String to send to echo server */
-  char echoBuffer[RCVBUFSIZE]; /* Buffer for echo string */
-  unsigned int echoStringLen; /* Length of string to echo */
-  int bytesRcvd, totalBytesRcvd; /* Bytes read in single recv() and total bytes read */
+  struct sockaddr_in serverAddr; /* Echo server address */
+  char option;
 
-  if ((argc < 3) || (argc > 4)) /* Test for correct number of arguments */
-  {
-    fprintf(stderr, "Usage: %s <Server IP> <Echo Word> [<Echo Port>]\n", argv[0]);
-    exit(1);
-  }
-
-  servlP = argv[1] ; /* First arg' server IP address (dotted quad) */
-  echoString = argv[2] ; /* Second arg' string to echo */
-
-  if (argc == 4)
-    echoServPort = atoi(argv[3]); /* Use given port, if any */
-  else
-    echoServPort = 7; /* 7 is the well-known port for the echo service */
-
-
-/* Create a reliable, stream socket using TCP */
+  /* Create a reliable, stream socket using TCP */
   if ((sock = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP)) < 0)
-  DieWithError(" socket () failed") ;
-
-
-/* Construct the server address structure */
-  memset(&echoServAddr, 0, sizeof(echoServAddr)); /* Zero out structure */
-  echoServAddr.sin_family = AF_INET; /* Internet address family */
-  echoServAddr.sin_addr.s_addr = inet_addr(servlP); /* Server IP address */
-  echoServAddr.sin_port = htons(echoServPort); /* Server port */
-
-
-/* Establish the connection to the echo server */
-  if (connect(sock, (struct sockaddr *) &echoServAddr, sizeof(echoServAddr)) < 0)
-    DieWithError(" connect () failed");
-
-  echoStringLen = strlen(echoString); /* Determine input length */
-
-
-/* Send the string to the server */
-  if (send(sock, echoString, echoStringLen, 0) != echoStringLen)
-    DieWithError("send() sent a different number of bytes than expected");
-
-
-/* Receive the same string back from the server */
-  totalBytesRcvd = 0;
-  printf("Received: "); /* Setup to print the echoed string */
-  while (totalBytesRcvd < echoStringLen)
+    DieWithError(" socket () failed") ;
+  
+  for(;;)
   {
-      
-    /* Receive up to the buffer size (minus i to leave space for a null terminator) bytes from the sender */
-    if ((bytesRcvd = recv(sock, echoBuffer, RCVBUFSIZE - 1, 0)) <= 0)
-      DieWithError("recv() failed or connection closed prematurely");
-    totalBytesRcvd += bytesRcvd; /* Keep tally of total bytes */
-    echoBuffer[bytesRcvd] = '\0'; /* Terminate the string! */
-    printf(echoBuffer); /* Print the echo buffer */
+    PrintMenuOptions();
+    scanf("%c", &option);
+    while(getchar() != '\n');
+    
+    int error;
+    switch(option) {
+      case '0': 
+        ConnectToServer(sock, serverAddr);
+        break;
+      case '1': 
+        printf("you entered: %c\n", option);
+        break;
+      case '2': 
+        printf("you entered: %c\n", option);
+        break;
+      case '3': 
+        printf("you entered: %c\n", option);
+        break;
+      case '4': 
+        printf("you entered: %c\n", option);
+        break;
+      case '5': 
+        printf("you entered: %c\n", option);
+        break;
+      default:
+        printf("Invalid option\n");
+    }
   }
-
-  printf("\n"); /* Print a final linefeed */
-
+  
   close(sock);
   exit(0);
+}
+
+void PrintMenuOptions(void) {
+  printf("----------------------------------------\n");
+  printf("Command:\n");
+  printf("0. Connect to the server\n");
+  printf("1. Get the user list\n");
+  printf("2. Send a message\n");
+  printf("3. Get my messages\n");
+  printf("4. Initiate a chat with my friend\n");
+  printf("5. Chat with my friend\n");
+  printf("Your option<enter a number>: ");
+}
+
+void ConnectToServer(int sock, struct sockaddr_in serverAddr) 
+{
+  const int LEN = 20;
+  char serverIP[LEN];
+  char port[LEN];
+  int serverPort;
+  
+  printf("Enter the IP address: ");
+  fgets(serverIP, sizeof(serverIP), stdin);
+  
+  printf("Enter the port number: ");
+  fgets(port, sizeof(port), stdin);
+  serverPort = atoi(port);
+  
+  /* Construct the server address structure */
+  memset(&serverAddr, 0, sizeof(serverAddr)); /* Zero out structure */
+  serverAddr.sin_family = AF_INET; /* Internet address family */
+  serverAddr.sin_addr.s_addr = inet_addr(serverIP); /* Server IP address */
+  serverAddr.sin_port = htons(serverPort); /* Server port */
+
+
+  /* Establish the connection to the echo server */
+  if (connect(sock, (struct sockaddr *) &serverAddr, sizeof(serverAddr)) < 0)
+    DieWithError(" connect () failed");
+
 }
